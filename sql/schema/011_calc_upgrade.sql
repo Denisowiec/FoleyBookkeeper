c46399093f81aab2a8da324c8400bf945ce188d4
Denisowiec <ddspeed@gmail.com>
2025-12-02 20:14:55 +0100

    Fixed calc_upgrade migration

--- sql/schema/011_calc_upgrade.sql
+++ sql/schema/011_calc_upgrade.sql
@@ -1,7 +1,11 @@
 -- +goose Up
 ALTER TABLE calculations ADD boss_tribute NUMERIC NOT NULL DEFAULT 30;
 ALTER TABLE calculations ADD manager_commission NUMERIC NOT NULL DEFAULT 3;
+ALTER TABLE calculations ADD tax_rate NUMERIC NOT NULL DEFAULT 0.12;
+ALTER TABLE calculations ADD tax_multiplier NUMERIC NOT NULL DEFAULT 0.5;
 
 -- +goose Down
 ALTER TABLE calculations DROP COLUMN boss_tribute;
 ALTER TABLE calculations DROP COLUMN manager_commission;
+ALTER TABLE calculations DROP COLUMN tax_rate;
+ALTER TABLE calculations DROP COLUMN tax_multiplier;
